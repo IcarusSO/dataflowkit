9d8efe53da9cdd406cdc224403d2cdd0be4ccf19
Icarus <icarus.so.ch@gmail.com>
2017-01-16 17:44:37 +0800

    initializing

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Icarus So',
   author_email = 'icarus.so@gmail.com',
   url = 'https://github.com/IcarusSO/dataflowkit', # use the URL to the github repo
-  download_url = 'https://github.com/IcarusSO/dataflow-management/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/IcarusSO/dataflowkit/tarball/0.1', # I'll explain this in a second
   keywords = ['dataflow', 'data', 'flow', 'management'], # arbitrary keywords
   classifiers = [],
 )
