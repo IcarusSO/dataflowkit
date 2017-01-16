e56a6d1d3b7864cb943b598bd4dea91c30e760c4
Icarus <icarus.so.ch@gmail.com>
2017-01-16 17:05:08 +0800

    initializing

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dfm',
+  packages = ['dfm'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A dataflow management framework for datascientists and engineers',
+  author = 'Icarus So',
+  author_email = 'icarus.so@gmail.com',
+  url = 'https://github.com/IcarusSO/dataflow-management', # use the URL to the github repo
+  download_url = 'https://github.com/IcarusSO/dataflow-management/tarball/0.1', # I'll explain this in a second
+  keywords = ['dataflow', 'data', 'flow', 'management'], # arbitrary keywords
+  classifiers = [],
+)
