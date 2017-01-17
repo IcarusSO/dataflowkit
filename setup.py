d7045a65a2d849de036bdcdc59539ecfa394657c
Icarus <icarus.so.ch@gmail.com>
2017-01-17 12:28:12 +0800

    initializing

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'dataflowkit',
-  packages = ['dataflowkit'], # this must be the same as the name above
-  version = '0.1',
+  packages = ['dataflowkit', 'dataflowkit.graphs', 'dataflowkit.datasets', 'dataflowkit.recipes', 'dataflowkit.utils'], # this must be the same as the name above
+  version = '0.1.2',
   description = 'A dataflow management framework for datascientists and engineers',
   author = 'Icarus So',
   author_email = 'icarus.so@gmail.com',
   url = 'https://github.com/IcarusSO/dataflowkit', # use the URL to the github repo
-  download_url = 'https://github.com/IcarusSO/dataflowkit/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/IcarusSO/dataflowkit/tarball/0.1.2', # I'll explain this in a second
   keywords = ['dataflow', 'data', 'flow', 'management'], # arbitrary keywords
   classifiers = [],
 )
